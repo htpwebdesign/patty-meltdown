2e7ae62ec36d65534c519e00ba5435a384d74243
J-A-R-S-H <122942384+J-A-R-S-H@users.noreply.github.com>
2024-03-20 10:22:28 -0700

    fixed acf location template part

--- template-parts/location.php
+++ template-parts/location.php
@@ -4,8 +4,8 @@
 
 <section class="map">
     <?php
-    if (function_exists('get_field') && get_field('contact_map')) {
-        the_field('contact_map');
+    if (function_exists('get_field') && get_field('contact_map', 23)) {
+        the_field('contact_map', 23);
     } else {
         echo '<p>Map not available.</p>';
     }
@@ -15,21 +15,21 @@
 <section class="contact-locations">
 
     <?php
-    if (have_rows('contact_locations')) :
-        while (have_rows('contact_locations')) : the_row();
+    if (have_rows('contact_locations', 23)) :
+        while (have_rows('contact_locations', 23)) : the_row();
             // Output your repeater field values here
 
             echo "<p>";
-            the_sub_field('location_name');
+            the_sub_field('location_name', 23);
             echo "</p>";
             echo "<p>";
-            the_sub_field('location_address');
+            the_sub_field('location_address', 23);
             echo "</p>";
             echo "<p>";
-            the_sub_field('location_hours');
+            the_sub_field('location_hours', 23);
             echo "</p>";
             echo "<p>";
-            the_sub_field('location_phone');
+            the_sub_field('location_phone', 23);
             echo "</p>";
 
         endwhile;
