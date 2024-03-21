725930f1aea4e3a6d9ac833b868410d42b95377e
J-A-R-S-H <122942384+J-A-R-S-H@users.noreply.github.com>
2024-03-20 18:09:36 -0700

    deleted some useless lines

--- functions.php
+++ functions.php
@@ -76,17 +76,6 @@ function patty_meltdown_setup()
 		)
 	);
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support(
-		'custom-background',
-		apply_filters(
-			'patty_meltdown_custom_background_args',
-			array(
-				'default-color' => 'ffffff',
-				'default-image' => '',
-			)
-		)
-	);
 
 	// Add theme support for selective refresh for widgets.
 	add_theme_support('customize-selective-refresh-widgets');
@@ -126,22 +115,6 @@ add_action('after_setup_theme', 'patty_meltdown_content_width', 0);
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function patty_meltdown_widgets_init()
-{
-	register_sidebar(
-		array(
-			'name'          => esc_html__('Sidebar', 'patty-meltdown'),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__('Add widgets here.', 'patty-meltdown'),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action('widgets_init', 'patty_meltdown_widgets_init');
-
 /**
  * Enqueue scripts and styles.
  */
