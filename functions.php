b0748605b517b5fc6ccd4d2f946c328946b25e19
arielle-m <ariem.marii@gmail.com>
2024-03-24 18:57:20 -0700

    created custom styles for the login page

--- functions.php
+++ functions.php
@@ -249,3 +249,39 @@ function my_acf_google_map_api($api)
 	return $api;
 }
 add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
+
+
+/* 
+Log In Customization
+*/
+
+// Load in custom login styles and scripts
+function pmd_login_stylesheet() {
+	wp_enqueue_style( 
+		'custom-login', 
+		get_stylesheet_directory_uri() . '/css/style-login.css',
+		array(),
+		_S_VERSION,
+	);
+
+	// wp_enqueue_script(
+	// 	'custom-login',
+	// 	get_stylesheet_directory_uri() . '/js/style-login.js',
+	// 	array(),
+	// 	_S_VERSION,
+	// 	array( 'strategy' => 'defer' ),
+	// );
+}
+add_action( 'login_enqueue_scripts', 'pmd_login_stylesheet' );
+
+// Custom login logo URL
+function pmd_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'pmd_login_logo_url' );
+
+// Custom login URL title
+function pmd_login_logo_url_title() {
+    return 'Patty Meltdown | Meltdowns are fine as long as it\'s cheesy!';
+}
+add_filter( 'login_headertext', 'pmd_login_logo_url_title' );
