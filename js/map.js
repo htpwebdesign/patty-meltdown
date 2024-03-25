a946ea6110ad9374b133f1e0c4704deb72deaca6
NandoHsp <fhospina@my.bcit.ca>
2024-03-25 14:51:53 -0700

    fixed ACF google map.

---
+++ js/map.js
@@ -0,0 +1,318 @@
+(function ($) {
+  /**
+   * initMap
+   *
+   * Renders a Google Map onto the selected jQuery element
+   *
+   * @date    22/10/19
+   * @since   5.8.6
+   *
+   * @param   jQuery $el The jQuery element.
+   * @return  object The map instance.
+   */
+  function initMap($el) {
+    // Find marker elements within map.
+    var $markers = $el.find(".marker");
+
+    var mapArgs = {
+      zoom: $el.data("zoom") || 16,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      // styles: [
+      //     {
+      //         "elementType": "labels",
+      //         "stylers": [
+      //             {
+      //                 "visibility": "off"
+      //             },
+      //             {
+      //                 "color": "#f49f53"
+      //             }
+      //         ]
+      //     },
+      //     {
+      //         "featureType": "landscape",
+      //         "stylers": [
+      //             {
+      //                 "color": "#f9ddc5"
+      //             },
+      //             {
+      //                 "lightness": -7
+      //             }
+      //         ]
+      //     },
+      //     {
+      //         "featureType": "road",
+      //         "stylers": [
+      //             {
+      //                 "color": "#813033"
+      //             },
+      //             {
+      //                 "lightness": 43
+      //             }
+      //         ]
+      //     },
+      //     {
+      //         "featureType": "poi.business",
+      //         "stylers": [
+      //             {
+      //                 "color": "#645c20"
+      //             },
+      //             {
+      //                 "lightness": 38
+      //             }
+      //         ]
+      //     },
+      //     {
+      //         "featureType": "water",
+      //         "stylers": [
+      //             {
+      //                 "color": "#1994bf"
+      //             },
+      //             {
+      //                 "saturation": -69
+      //             },
+      //             {
+      //                 "gamma": 0.99
+      //             },
+      //             {
+      //                 "lightness": 43
+      //             }
+      //         ]
+      //     },
+      //     {
+      //         "featureType": "road.local",
+      //         "elementType": "geometry.fill",
+      //         "stylers": [
+      //             {
+      //                 "color": "#f19f53"
+      //             },
+      //             {
+      //                 "weight": 1.3
+      //             },
+      //             {
+      //                 "visibility": "on"
+      //             },
+      //             {
+      //                 "lightness": 16
+      //             }
+      //         ]
+      //     },
+      //     {
+      //         "featureType": "poi.business"
+      //     },
+      //     {
+      //         "featureType": "poi.park",
+      //         "stylers": [
+      //             {
+      //                 "color": "#645c20"
+      //             },
+      //             {
+      //                 "lightness": 39
+      //             }
+      //         ]
+      //     },
+      //     {
+      //         "featureType": "poi.school",
+      //         "stylers": [
+      //             {
+      //                 "color": "#a95521"
+      //             },
+      //             {
+      //                 "lightness": 35
+      //             }
+      //         ]
+      //     },
+      //     {},
+      //     {
+      //         "featureType": "poi.medical",
+      //         "elementType": "geometry.fill",
+      //         "stylers": [
+      //             {
+      //                 "color": "#813033"
+      //             },
+      //             {
+      //                 "lightness": 38
+      //             },
+      //             {
+      //                 "visibility": "off"
+      //             }
+      //         ]
+      //     },
+      //     {},
+      //     {},
+      //     {},
+      //     {},
+      //     {},
+      //     {},
+      //     {},
+      //     {},
+      //     {},
+      //     {},
+      //     {},
+      //     {
+      //         "elementType": "labels"
+      //     },
+      //     {
+      //         "featureType": "poi.sports_complex",
+      //         "stylers": [
+      //             {
+      //                 "color": "#9e5916"
+      //             },
+      //             {
+      //                 "lightness": 32
+      //             }
+      //         ]
+      //     },
+      //     {},
+      //     {
+      //         "featureType": "poi.government",
+      //         "stylers": [
+      //             {
+      //                 "color": "#9e5916"
+      //             },
+      //             {
+      //                 "lightness": 46
+      //             }
+      //         ]
+      //     },
+      //     {
+      //         "featureType": "transit.station",
+      //         "stylers": [
+      //             {
+      //                 "visibility": "off"
+      //             }
+      //         ]
+      //     },
+      //     {
+      //         "featureType": "transit.line",
+      //         "stylers": [
+      //             {
+      //                 "color": "#813033"
+      //             },
+      //             {
+      //                 "lightness": 22
+      //             }
+      //         ]
+      //     },
+      //     {
+      //         "featureType": "transit",
+      //         "stylers": [
+      //             {
+      //                 "lightness": 38
+      //             }
+      //         ]
+      //     },
+      //     {
+      //         "featureType": "road.local",
+      //         "elementType": "geometry.stroke",
+      //         "stylers": [
+      //             {
+      //                 "color": "#f19f53"
+      //             },
+      //             {
+      //                 "lightness": -10
+      //             }
+      //         ]
+      //     },
+      //     {},
+      //     {},
+      //     {}
+      // ]
+    };
+    var map = new google.maps.Map($el[0], mapArgs);
+
+    // Add markers.
+    map.markers = [];
+    $markers.each(function () {
+      initMarker($(this), map);
+    });
+
+    // Center map based on markers.
+    centerMap(map);
+
+    // Return map instance.
+    return map;
+  }
+
+  /**
+   * initMarker
+   *
+   * Creates a marker for the given jQuery element and map.
+   *
+   * @date    22/10/19
+   * @since   5.8.6
+   *
+   * @param   jQuery $el The jQuery element.
+   * @param   object The map instance.
+   * @return  object The marker instance.
+   */
+  function initMarker($marker, map) {
+    // Get position from marker.
+    var lat = $marker.data("lat");
+    var lng = $marker.data("lng");
+    var latLng = {
+      lat: parseFloat(lat),
+      lng: parseFloat(lng),
+    };
+
+    // Create marker instance.
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map,
+    });
+
+    // Append to reference for later use.
+    map.markers.push(marker);
+
+    // If marker contains HTML, add it to an infoWindow.
+    if ($marker.html()) {
+      // Create info window.
+      var infowindow = new google.maps.InfoWindow({
+        content: $marker.html(),
+      });
+
+      // Show info window when marker is clicked.
+      google.maps.event.addListener(marker, "click", function () {
+        infowindow.open(map, marker);
+      });
+    }
+  }
+
+  /**
+   * centerMap
+   *
+   * Centers the map showing all markers in view.
+   *
+   * @date    22/10/19
+   * @since   5.8.6
+   *
+   * @param   object The map instance.
+   * @return  void
+   */
+  function centerMap(map) {
+    // Create map boundaries from all map markers.
+    var bounds = new google.maps.LatLngBounds();
+    map.markers.forEach(function (marker) {
+      bounds.extend({
+        lat: marker.position.lat(),
+        lng: marker.position.lng(),
+      });
+    });
+
+    // Case: Single marker.
+    if (map.markers.length == 1) {
+      map.setCenter(bounds.getCenter());
+
+      // Case: Multiple markers.
+    } else {
+      map.fitBounds(bounds);
+    }
+  }
+
+  // Render maps on page load.
+  $(document).ready(function () {
+    $(".acf-map").each(function () {
+      var map = initMap($(this));
+    });
+  });
+})(jQuery);
