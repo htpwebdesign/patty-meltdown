5c5b9d60a687177e4fb810be4b8de8fff747afba
arielle-m <ariem.marii@gmail.com>
2024-03-31 19:09:16 -0700

    fixed site nav's full bleed

--- header.php
+++ header.php
@@ -28,42 +28,44 @@
 		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'patty-meltdown'); ?></a>
 
 		<header id="masthead" class="site-header full-bleed">
-			<div class="site-branding">
-				<?php
-				the_custom_logo();
-				if (is_front_page() && is_home()) :
-					?>
-					<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+			<div class="header-content">
+				<div class="site-branding">
 					<?php
-				else :
+					the_custom_logo();
+					if (is_front_page() && is_home()) :
+						?>
+						<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+						<?php
+					else :
+						?>
+						<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
+						<?php
+					endif;
 					?>
-					<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
-					<?php
-				endif;
-				?>
-			</div><!-- .site-branding -->
+				</div><!-- .site-branding -->
 
-			<nav id="site-navigation" class="main-navigation">
-				<button class="menu-toggle" aria-controls="header-menu" aria-expanded="false" aria-label="Menu Toggle">
-					<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
-						<title>Menu icon</title>
-						<path d="M2 7h20"></path>
-						<path d="M2 12h20"></path>
-						<path d="M2 17h20"></path>
-					</svg>
-				</button>
-				<div class="header-nav">
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'header',
-							'menu_id'        => 'header-menu',
-						)
-					);
-					if (function_exists('patty_meltdown_woocommerce_header_cart')) {
-						patty_meltdown_woocommerce_header_cart();
-					}
-					?>
-				</div>
-			</nav><!-- #site-navigation -->
+				<nav id="site-navigation" class="main-navigation">
+					<button class="menu-toggle" aria-controls="header-menu" aria-expanded="false" aria-label="Menu Toggle">
+						<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
+							<title>Menu icon</title>
+							<path d="M2 7h20"></path>
+							<path d="M2 12h20"></path>
+							<path d="M2 17h20"></path>
+						</svg>
+					</button>
+					<div class="header-nav">
+						<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'header',
+								'menu_id'        => 'header-menu',
+							)
+						);
+						if (function_exists('patty_meltdown_woocommerce_header_cart')) {
+							patty_meltdown_woocommerce_header_cart();
+						}
+						?>
+					</div>
+				</nav><!-- #site-navigation -->
+			</div>
 		</header><!-- #masthead -->
