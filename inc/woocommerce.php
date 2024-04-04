155178041ead54a0def9865004da57233c152697
J-A-R-S-H <122942384+J-A-R-S-H@users.noreply.github.com>
2024-04-04 12:12:21 -0700

    accepted stash

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -267,8 +267,6 @@ function woocommerce_template_single_excerpt()
 {
 	return;
 }
-<<<<<<< Updated upstream
-=======
 
 
 add_action('woocommerce_before_single_product_summary', 'add_byob_image', 21);
@@ -278,4 +276,3 @@ function add_byob_image()
 {
 	echo "<p>yes</p>";
 }
->>>>>>> Stashed changes
